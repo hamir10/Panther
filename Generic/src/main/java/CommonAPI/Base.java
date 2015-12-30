44538999632a8b0c14849f7c12999cfcd6966bf5
Heidar Amirmehrabi <mehrabi.ca@gmail.com>
2015-12-30 17:58:31 -0500

    Base code without sleep sync

--- Generic/src/main/java/CommonAPI/Base.java
+++ Generic/src/main/java/CommonAPI/Base.java
@@ -1,13 +1,17 @@
 package CommonAPI;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import org.testng.annotations.Parameters;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -46,12 +50,67 @@ public class Base {
 
     }
 
-    public void typeByXpath(String locator, String value){
-        driver.findElement(By.xpath(locator)).sendKeys(value);
+    public void typeBycssThenEnter(String locator, String value){
+        driver.findElement(By.cssSelector(locator)).sendKeys(Keys.CLEAR, value, Keys.ENTER);
 
     }
 
-    public void enterkeys(){
+    public void typeByXpath(String locator){
+        driver.findElement(By.xpath(locator)).click();
 
+    }
+
+    public void clickById(String locator){
+        driver.findElement(By.id(locator)).click();
+
+    }
+
+    public void clickByName(String locator){
+        driver.findElement(By.id(locator)).click();
+
+    }
+
+    public void navigateBack(){
+        driver.navigate().back();
+    }
+
+    public void navigateForward(){
+        driver.navigate().forward();
+    }
+
+    public String getTextByCss(String locator){
+        String st = driver.findElement(By.cssSelector(locator)).getText();
+        return st;
+    }
+
+    public String getTextByXpath(String locator){
+        String st = driver.findElement(By.xpath(locator)).getText();
+        return st;
+    }
+
+    public String getTextById(String locator){
+        String st = driver.findElement(By.id(locator)).getText();
+        return st;
+    }
+
+    public String getTextByName(String locator){
+        String st = driver.findElement(By.name(locator)).getText();
+        return st;
+    }
+
+    public WebElement getWebElementByCss(String locator){
+        WebElement element = driver.findElement(By.cssSelector(locator));
+        return element;
+    }
+
+    public List<WebElement> getWebElementsByCss(String locator){
+        List<WebElement> elementList = new ArrayList<WebElement>();
+        elementList = driver.findElements(By.cssSelector(locator));
+        return elementList;
+    }
+
+    public WebElement getWebElementByXpath(String locator){
+        WebElement element = driver.findElement(By.xpath(locator));
+        return element;
     }
 }
